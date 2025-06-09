9b0ff855e6dc66ad4b1ea5c04b59981a952f9f11
Vinícius Ross <108466053+viniross@users.noreply.github.com>
2025-06-09 15:57:44 -0300

    Variables Evento class created

--- src/Evento.java
+++ src/Evento.java
@@ -1,3 +1,10 @@
 public class Evento {
-    
+    private int codigo;
+    private String nome;
+    private String tipo;
+    private String dataInicial;
+    private String dataFinal;
+    private int quantSalas;
+    private Salas[] listaSalas;
+    private Inscricoes[] listaInscricoes;
 }
